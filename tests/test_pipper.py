a9bb00bf79ba39b871137f6633f85a013737eae3
ddelange <14880945+ddelange@users.noreply.github.com>
2020-06-30 14:22:53 +0200

    :white_check_mark: Add test for dot in pip10

--- tests/test_pipper.py
+++ tests/test_pipper.py
@@ -11,6 +11,19 @@ from pipgrip.pipper import _download_wheel, _get_available_versions
     "package, pip_output, expected",
     [
         (
+            ".[all]",
+            """
+            Collecting jupyterlab-black
+              Downloading https://files.pythonhosted.org/packages/b3/c9/b3d38a0cc2a5237becb3c2f8843ca5a8e884906e9018029e4c4e5c43f62e/jupyterlab_black-0.2.1.tar.gz
+            Building wheels for collected packages: jupyterlab-black
+              Running setup.py bdist_wheel for jupyterlab-black: started
+              Running setup.py bdist_wheel for jupyterlab-black: finished with status 'done'
+              Stored in directory: ~/Library/Caches/pip/wheels/pipgrip
+            Successfully built jupyterlab-black
+            """,
+            "jupyterlab_black-0.2.1-py3-none-any.whl",
+        ),
+        (
             "six",
             """
             Collecting six
@@ -76,29 +89,15 @@ from pipgrip.pipper import _download_wheel, _get_available_versions
             """,
             "jupyterlab_black-0.2.1-py3-none-any.whl",
         ),
-        (
-            ".[all]",
-            """
-            Collecting jupyterlab-black
-              Downloading jupyterlab_black-0.2.1.tar.gz (3.1 kB)
-            Building wheels for collected packages: jupyterlab-black
-              Building wheel for jupyterlab-black (setup.py): started
-              Building wheel for jupyterlab-black (setup.py): finished with status 'done'
-              Created wheel for jupyterlab-black: filename=jupyterlab_black-0.2.1-py3-none-any.whl size=2497 sha256=2d21a5420b39156f7e55da105b8a064889674ae8a1a09f3fd2884c78a994a851
-              Stored in directory: ~/Library/Caches/pip/wheels/83/ba/78/469d847858dff4d2e600bff2de9d09bf455bb5be3ffb566af1
-            Successfully built jupyterlab-black
-            """,
-            "jupyterlab_black-0.2.1-py3-none-any.whl",
-        ),
     ],
     ids=(
+        "pip10 .",
         "pip10 fetched 1",
         "pip10 built 1",
         "pip>10 cached 1",
         "pip>10 cached 2",
         "pip>10 fetched 2",
         "pip>10 built 1",
-        ".",
     ),
 )
 def test_download_wheel(package, pip_output, expected, monkeypatch):
