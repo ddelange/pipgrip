9f3bcdbd32fb637003825ae887b8f70ca54e8283
ddelange <14880945+ddelange@users.noreply.github.com>
2022-11-22 09:10:58 +0100

    :construction_worker: Add python 3.11

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Archiving :: Packaging",
         "Topic :: System :: Systems Administration",
