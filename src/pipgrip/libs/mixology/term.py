d7fe0c7b83953dc19c77897c47c6210716231ca6
ddelange <14880945+ddelange@users.noreply.github.com>
2024-01-10 19:30:40 +0100

    :page_facing_up: Add copyright notice to all .py files (#136)
    * :page_facing_up: Add copyright notice to all .py files
    
    * Remove non-ascii char
    
    * Remove non-ascii chars
    
    * Remove non-ascii chars

--- src/pipgrip/libs/mixology/term.py
+++ src/pipgrip/libs/mixology/term.py
@@ -1,4 +1,35 @@
-# -*- coding: utf-8 -*-
+# BSD 3-Clause License
+#
+# Copyright (c) 2020 - 2024, ddelange, <ddelange@delange.dev>
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# SPDX-License-Identifier: BSD-3-Clause
 from typing import Hashable, Optional
 
 from pipgrip.libs.mixology.constraint import Constraint
@@ -124,7 +155,7 @@ class Term(object):
 
         if self.is_compatible_with(other):
             if self.is_positive() != other.is_positive():
-                # foo ^1.0.0 ∩ not foo ^1.5.0 → foo >=1.0.0 <1.5.0
+                # foo ^1.0.0 n not foo ^1.5.0 -> foo >=1.0.0 <1.5.0
                 positive = self if self.is_positive() else other
                 negative = other if self.is_positive() else self
 
@@ -132,12 +163,12 @@ class Term(object):
                     positive.constraint.difference(negative.constraint), True
                 )
             elif self.is_positive():
-                # foo ^1.0.0 ∩ foo >=1.5.0 <3.0.0 → foo ^1.5.0
+                # foo ^1.0.0 n foo >=1.5.0 <3.0.0 -> foo ^1.5.0
                 to_return = self._non_empty_term(
                     self.constraint.intersect(other.constraint), True
                 )
             else:
-                # not foo ^1.0.0 ∩ not foo >=1.5.0 <3.0.0 → not foo >=1.0.0 <3.0.0
+                # not foo ^1.0.0 n not foo >=1.5.0 <3.0.0 -> not foo >=1.0.0 <3.0.0
                 to_return = self._non_empty_term(
                     self.constraint.union(other.constraint), False
                 )
